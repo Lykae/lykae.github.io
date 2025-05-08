e1a65eda275ee3500a228d3d63439ab645a02ba4
Mandl-Laptop <zwerg01@gmx.net>
2025-05-08 20:35:41 +0200

    new gamemode

--- tripeaks_rush/flutter_bootstrap.js
+++ tripeaks_rush/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1684770975"
+    serviceWorkerVersion: "2750882760"
   }
 });
